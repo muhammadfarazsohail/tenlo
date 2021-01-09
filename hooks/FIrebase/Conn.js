35e8f7aea3144b8fc1a26c356c94268932880190
Muhammad Faraz Sohail <mfg_so115@hotmail.com>
2021-01-08 20:23:56 -0500

    updated the login page

---
+++ hooks/FIrebase/Conn.js
@@ -0,0 +1,26 @@
+<div>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js"></script>
+
+{/* <!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries --> */}
+<script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-analytics.js"></script>
+
+<script>
+  {/* // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional */}
+  var firebaseConfig = {
+    apiKey: "AIzaSyCRFEKyWJ3ajV-TiXCaXtD0DdqO3g75CyA",
+    authDomain: "tenlo-project.firebaseapp.com",
+    databaseURL: "https://tenlo-project-default-rtdb.firebaseio.com",
+    projectId: "tenlo-project",
+    storageBucket: "tenlo-project.appspot.com",
+    messagingSenderId: "826972459172",
+    appId: "1:826972459172:web:9e56eaa69a32eeccf8d631",
+    measurementId: "G-DLXVLF3G3F"
+  };
+  {/* // Initialize Firebase */}
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+</div>
